f39f0fae655ab852a68c9b31686d189ca2c852b6
Ruona Dibie <ogheneruona.dibie@gmail.com>
2020-08-06 12:02:51 -0700

    used Aux and capitalized first letter ingredient

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,17 +1,21 @@
 import React from "react";
+import Aux from "../../../hoc/Aux";
 const OrderSummary = (props) => {
-  const ingredientSummary = Object.entries(
-    props.ingredients
-  ).map((ingredient) => (
-    <li key={ingredient[0]}>{`${ingredient[0]} : ${ingredient[1]}`}</li>
-  ));
+  const ingredientSummary = Object.entries(props.ingredients).map(
+    (ingredient) => (
+      <li key={ingredient[0]}>
+        <span style={{ textTransform: "capitalize" }}>{ingredient[0]}</span> :{" "}
+        {ingredient[1]}
+      </li>
+    )
+  );
   return (
-    <React.Fragment>
+    <Aux>
       <h3>Your Order</h3>{" "}
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
-    </React.Fragment>
+    </Aux>
   );
 };
 
