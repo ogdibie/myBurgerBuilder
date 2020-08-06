73a54e2e4bb6938309df44bc9a993d76f6501f10
Ruona Dibie <ogheneruona.dibie@gmail.com>
2020-08-06 16:27:13 -0700

    Added button for order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Aux from "../../../hoc/Aux";
+import Button from "../../UI/Button/Button";
 const OrderSummary = (props) => {
   const ingredientSummary = Object.entries(props.ingredients).map(
     (ingredient) => (
@@ -14,7 +15,16 @@ const OrderSummary = (props) => {
       <h3>Your Order</h3>{" "}
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price : {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
+      <Button type="Danger" clicked={props.cancelPurchase}>
+        CANCEL
+      </Button>
+      <Button type="Success" clicked={props.continuePurchase}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
