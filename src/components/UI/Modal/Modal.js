41101b81cfde320c48403d54fc35f7c7bd81911f
Ruona Dibie <ogheneruona.dibie@gmail.com>
2020-08-06 16:28:33 -0700

    Added a backdrop component with click to hide modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,21 @@
 import React from "react";
 import classes from "./Modal.module.css";
+import Backdrop from "../Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
 const Modal = (props) => {
   return (
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0",
-      }}
-    >
-      {props.children}
-    </div>
+    <Aux>
+      <Backdrop show={props.show} clicked={props.cancelPurchase} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0",
+        }}
+      >
+        {props.children}
+      </div>
+    </Aux>
   );
 };
 
