8ff216dfb274f6bbce1ea32acfc17d02e3c1a38d
Ruona Dibie <ogheneruona.dibie@gmail.com>
2020-08-07 20:21:18 -0700

    Folder structured updated

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import Aux from "../../hoc/Aux";
+import Aux from "../Aux/Aux";
 import classes from "./Layout.module.css";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 const Layout = (props) => {
   const [showSideDrawer, setShowSideDrawer] = useState(false);
   const closeSideDrawerHandler = () => {
