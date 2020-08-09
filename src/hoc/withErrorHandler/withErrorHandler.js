4a8f470b62e11a7ae8c9d2f669f4f935754cc71e
Ruona Dibie <ogheneruona.dibie@gmail.com>
2020-08-08 23:57:23 -0700

    clean up the interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,16 +8,22 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null,
     };
     componentDidMount() {
-      axios.interceptors.request.use((req) => {
+      this.requestInterceptors = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.responseInterceptors = axios.interceptors.response.use(
         (res) => res,
         (error) => {
           this.setState({ error });
         }
       );
+    }
+
+    //removing the interceptors
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.requestInterceptors);
+      axios.interceptors.response.eject(this.responseInterceptors);
     }
 
     errorConfirmedHandler = () => {
